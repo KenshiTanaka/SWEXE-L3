74402881e952c0b0b91af1a08c5d8aa76df8be05
KenshiTanaka <1911140064f@ed.fuk.kindai.ac.jp>
2021-10-02 06:52:59 +0000

    ツイッターもどき一応完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,41 @@
 class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all
+        
+    end
     
+    def new 
+        @tweet = Tweet.new
+    end
+    
+    def create
+        @tweet = Tweet.new(message: params[:tweet][:message], tdate: Time.current)
+        if @tweet.save
+            flash[:notice] = 'ツイートしました'
+            redirect_to '/'
+        else
+            render new_tweets_path
+        end
+    end
+    
+    def destroy
+        tweet = Tweet.find(params[:id])
+        tweet.destroy
+        redirect_to root_path
+    end
+    
+    def edit
+        @tweet = Tweet.find(params[:id])
+    end
+    
+    def update
+        @tweet = Tweet.find(params[:id])
+        message = params[:tweet][:message]
+        @tweet.update(message: message)
+        redirect_to root_path
+    end
+    
+    def show 
+        @tweet = Tweet.find(params[:id])
+    end
 end
